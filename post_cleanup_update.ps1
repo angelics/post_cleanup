a8b5ac4d09297c4aed5264a5aea31668d11b0889
angelics <angelics@users.noreply.github.com>
2024-07-26 20:40:14 +0800

    Update post_cleanup_update.ps1

--- post_cleanup_update.ps1
+++ post_cleanup_update.ps1
@@ -358,6 +358,91 @@ Function Clear-NotepadPP
     }
 }
 
+function Clear-DuplicateOldDrivers {
+	#https://github.com/maxbakhub/winposh/blob/main/WindowsDesktopManagement/RemoveOldDuplicateDrivers.ps1
+	$dismOut = dism /online /get-drivers
+	$Lines = $dismOut | select -Skip 10
+	$Operation = "theName"
+	$Drivers = @()
+	foreach ( $Line in $Lines ) {
+		$tmp = $Line
+		$txt = $($tmp.Split( ':' ))[1]
+		switch ($Operation) {
+			'theName' { $Name = $txt
+						 $Operation = 'theFileName'
+						 break
+					   }
+			'theFileName' { $FileName = $txt.Trim()
+							 $Operation = 'theEntr'
+							 break
+						   }
+			'theEntr' { $Entr = $txt.Trim()
+						 $Operation = 'theClassName'
+						 break
+					   }
+			'theClassName' { $ClassName = $txt.Trim()
+							  $Operation = 'theVendor'
+							  break
+							}
+			'theVendor' { $Vendor = $txt.Trim()
+						   $Operation = 'theDate'
+						   break
+						 }
+			'theDate' { # we'll change the default date format for easy sorting
+						 $tmp = $txt.split( '.' )
+						 $txt = "$($tmp[2]).$($tmp[1]).$($tmp[0].Trim())"
+						 $Date = $txt
+						 $Operation = 'theVersion'
+						 break
+					   }
+			'theVersion' { $Version = $txt.Trim()
+							$Operation = 'theNull'
+							$params = [ordered]@{ 'FileName' = $FileName
+												  'Vendor' = $Vendor
+												  'Date' = $Date
+												  'Name' = $Name
+												  'ClassName' = $ClassName
+												  'Version' = $Version
+												  'Entr' = $Entr
+												}
+							$obj = New-Object -TypeName PSObject -Property $params
+							$Drivers += $obj
+							break
+						  }
+			 'theNull' { $Operation = 'theName'
+						  break
+						 }
+		}
+	}
+	$last = ''
+	$NotUnique = @()
+	foreach ( $Dr in $($Drivers | sort Filename) ) {
+		if ($Dr.FileName -eq $last  ) {  $NotUnique += $Dr  }
+		$last = $Dr.FileName
+	}
+	$NotUnique | sort FileName | ft
+	# search for duplicate drivers 
+	$list = $NotUnique | select -ExpandProperty FileName -Unique
+	$ToDel = @()
+	foreach ( $Dr in $list ) {
+		# Write-Host "duplicate driver found" -ForegroundColor Yellow
+		$sel = $Drivers | where { $_.FileName -eq $Dr } | sort date -Descending | select -Skip 1
+		$sel | ft
+		$ToDel += $sel
+	}
+	# Write-Host "List of driver version  to remove" -ForegroundColor Red
+	$ToDel | ft
+	# Removing old driver versions
+	# Uncomment the Invoke-Expression to automatically remove old versions of device drivers 
+	foreach ( $item in $ToDel ) {
+		$Name = $($item.Name).Trim()
+		#Write-Host "deleting $Name" -ForegroundColor Yellow
+		Write-Log "pnputil.exe /remove-device  $Name" -ForegroundColor Yellow
+		Invoke-Expression -Command "pnputil.exe /remove-device $Name"
+	}
+
+}
+
 function Get-WingetVersion {
     try {
         # Run winget --version and remove any leading/trailing whitespace and the "v" prefix
@@ -767,6 +852,8 @@ Function Araid-CleanAndRestart {
 	$unknown_devs | ForEach-Object {
 		Write-Log "$($_.InstanceId) has been removed"
 	}
+	
+	# Clear-DuplicateOldDrivers
 	
 	Start-Process cleanmgr.exe -ArgumentList "/d $env:homedrive" -Wait
 	Write-Log "cleanmgr /d $env:homedrive"
