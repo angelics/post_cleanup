f13e13cfd7247dcc33beae62cd8d2c92f86552ea
angelics <angelics@users.noreply.github.com>
2025-02-16 19:57:30 +0800

    Update post_cleanup_update.ps1

--- post_cleanup_update.ps1
+++ post_cleanup_update.ps1
@@ -989,6 +989,7 @@ Function Araid-CleanAndRestart {
 		"$env:systemroot\Web\Wallpaper\WALLPAPER.jpg",
 		"$env:systemroot\Web\Wallpaper\img0.jpg",
 		"$env:systemroot\Web\Wallpaper\Surface\Surface.jpg",
+		"$env:systemroot\Web\Wallpaper\MateBook\01.jpg",
 		"$env:systemroot\Web\Wallpaper\Windows\img0.jpg"
 	)
 
